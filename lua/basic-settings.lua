2ab127101818f7cf3c03c56cf82f56e95a1f0a81
Yuki XD <pxnditxyr@gmail.com>
2021-07-21 12:55:30 -0400

    Configure theme

--- lua/basic-settings.lua
+++ lua/basic-settings.lua
@@ -23,4 +23,10 @@ vim.g.clipboard = {
     cache_enabled = false
 }
 
+vim.g.tokyonight_style = "storm"
+
+-- vim.g.tokyonight_italic_functions = true
+-- vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+-- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
 vim.cmd[[colorscheme tokyonight]]
