3d35a4df4bd1941544a62067ac5bfd730d066000
Yuki XD <pxnditxyr@gmail.com>
2021-07-22 20:11:46 -0400

    Added maps to copy & cut, and description

--- lua/maps.lua
+++ lua/maps.lua
@@ -8,9 +8,17 @@ vim.g.mapleader = ' '
 cmd 'cnoreabbrev tree NERDTreeToggle'
 cmd 'cnoreabbrev find NERDTreeFind'
 
+-- Open NerdTree
+mapper( "n", "<Leader>nt", ":NERDTreeFind<CR>" )
 
+-- Save and quit
 mapper( "n", "<Leader>w", ":w<CR>" )
 mapper( "n", "<Leader>q", ":q<CR>" )
-mapper( "n", "<Leader>nt", ":NERDTreeFind<CR>" )
+
+-- Move in the split 
 mapper( "n", "<c-h>", "<c-w><c-h>")
 mapper( "n", "<c-l>", "<c-w><c-l>" )
+
+-- Copy and cut
+mapper( "n", "<Leader>aa", ":%y<CR>" )
+mapper( "n", "<Leader>a", ":%d<CR>" )
